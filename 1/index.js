5ec6d1d02daf0de5b448bc083dd709e1a5433ede
Jesse Croxford <jcroxford@simplenexus.com>
2021-11-30 22:12:17 -0700

    add solution & test text for day 1

--- 1/index.js
+++ 1/index.js
@@ -2,14 +2,35 @@
  * @param {string} input
  */
 function part1(input) {
-
+  return input
+    .split('\n')
+    .map(Number)
+    .reduce((total, next, i, depths) => {
+      if(!i) return total
+      if(depths[i-1] < next) return total + 1
+      return total
+    }, 0)
 }
 
 /**
  * @param {string} input
  */
 function part2(input) {
+  return input
+    .split('\n')
+    .map(Number)
+    .reduce((list, next, i, depths) => {
+      if(i >= depths.length - 2) return list
 
+      list.push(next + depths[i+1] + depths[i+2])
+
+      return list
+    }, [])
+    .reduce((total, next, i, depths) => {
+      if(!i) return total
+      if(depths[i-1] < next) return total + 1
+      return total
+    }, 0)
 }
 
 module.exports = { part1, part2 }
