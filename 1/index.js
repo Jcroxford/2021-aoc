bcd17fbd944023ef9d1cdfda070f165fce705e3d
Jesse Croxford <jcroxford@simplenexus.com>
2021-11-30 21:54:19 -0700

    refactor part 1 skeleton

--- 1/index.js
+++ 1/index.js
@@ -1,11 +1,15 @@
-module.exports = {
-  part1 (input) {
-    console.log('part 1 input: ', input);
+/**
+ * @param {string} input
+ */
+function part1(input) {
 
-  },
-
-  part2 (input) {
-    console.log('part 2 input: ', input);
-
-  }
 }
+
+/**
+ * @param {string} input
+ */
+function part2(input) {
+
+}
+
+module.exports = { part1, part2 }
