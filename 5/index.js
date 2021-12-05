bd9ef265cf8d06b0cd020fed2af96545d17a2927
Jesse Croxford <jcroxford@simplenexus.com>
2021-12-04 23:45:36 -0700

    change dot string to 0 to make code a bit cleaner again

--- 5/index.js
+++ 5/index.js
@@ -19,13 +19,13 @@ function findVents(includeDiagonals = false) {
             const highest = Math.max(y1, y2)
             const lowest = Math.min(y1, y2)
             for(let i = lowest; i <= highest; i++) {
-              grid[i][x1] = grid[i][x1] == '.' ? 1 : grid[i][x1] + 1
+              grid[i][x1]++
             }
           } else if(y1 == y2) {
             const highest = Math.max(x1, x2)
             const lowest = Math.min(x1, x2)
             for(let i = lowest; i <= highest; i++) {
-              grid[y1][i] = grid[y1][i] == '.' ? 1 : grid[y1][i] + 1
+              grid[y1][i]++
             }
           } else if(includeDiagonals) {
             const largestDistance = Math.max(Math.abs(x1 - x2), Math.abs(y1 - y2))
@@ -34,7 +34,7 @@ function findVents(includeDiagonals = false) {
             const yDirection = currentY == Math.max(y1, y2) ? -1 : 1
 
             for (let i = 0; i <= largestDistance; i++) {
-              grid[currentY][currentX] = grid[currentY][currentX] == '.' ? 1 : grid[currentY][currentX] + 1
+              grid[currentY][currentX]++
               currentX++
               currentY = currentY + yDirection
             }
@@ -42,10 +42,10 @@ function findVents(includeDiagonals = false) {
 
           return grid
         },
-        new Array(GRID_SIZE).fill(null).map(x => new Array(GRID_SIZE).fill('.')) // change arra
+        new Array(GRID_SIZE).fill(null).map(x => new Array(GRID_SIZE).fill(0)) // change arra
       )
         // .map(x => x.join('')).join('\n') // to visualize graph comment this in and comment out below lines
-      .map(y => y.filter(x => x != '.' && x > 1))
+      .map(y => y.filter(x => x > 1))
       .flat()
       .length
   }
